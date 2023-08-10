3a209f545e71a4fbe1356211c741f63bf578cba6
Aliye Idris <aliyeidiris@gmail.com>
2023-08-10 15:56:39 +0300

    Junit test2

--- MyPracticeProject/src/test/java/regressionsuit/junitframework/JunitTestDemo1.java
+++ MyPracticeProject/src/test/java/regressionsuit/junitframework/JunitTestDemo1.java
@@ -1,36 +1,43 @@
 package regressionsuit.junitframework;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.*;
 
 public class JunitTestDemo1 {
     @BeforeAll
     public static void setUp(){
-        System.out.println("Before All run once before all tests");
+        System.out.println("BeforeClass run once before all tests");
+    }
+    @BeforeEach
+    public  void start(){
+        System.out.println("Test started.");
+    }
+    @AfterEach
+    public  void end(){
+        System.out.println("Test ended.");
     }
     @AfterAll
     public static void tearDown(){
-        System.out.println("After All reun once after all tests");
+        System.out.println("AfterClass run once after all tests");
     }
 
     @Test
     public void squareRootTest(){
         System.out.println("Testing a square root");
-        Assert.assertTrue(Math.sqrt(9)==3);
+        Assertions.assertTrue(Math.sqrt(9)==3);
     }
+    @Disabled
     @Test
     public void compareTwoString(){
         String s1="selenium";
         String s2="selenium";
-        Assert.assertEquals(s1,s2);
+        Assertions.assertEquals(s1,s2);
+        System.out.println("Testing two strings");
 
     }
-    @Test
+    @RepeatedTest(3)
     public void power(){
         System.out.println("Testing the power of a number");
-        Assert.assertTrue(Math.pow(10,2)==100);
+        Assertions.assertTrue(Math.pow(10,2)==100);
     }
 
 }
