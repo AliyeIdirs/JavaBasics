342f7fc9f3c73a7261c1f5d01a1ca0d8e3a24cf3
Aliye Idris <58428620+AliyeIdris@users.noreply.github.com>
2023-08-22 16:36:51 +0300

    Delete src directory

--- src/main/java/com/unitedcoder/classconcepts/package2/Gamma.java
+++
@@ -1,7 +0,0 @@
-package com.unitedcoder.classconcepts.package2;
-
-import com.unitedcoder.classconcepts.package1.Alpha;
-
-public class Gamma {
-    Alpha alpha = new Alpha();
-}
--- src/main/java/com/unitedcoder/oopsconcepts/abstracttutorial/HSBCBank.java
+++
@@ -1,8 +0,0 @@
-package com.unitedcoder.oopsconcepts.abstracttutorial;
-
-public class HSBCBank extends Bank{
-    @Override
-    public void loan() {
-        System.out.println("Bank Loan");
-    }
-}
