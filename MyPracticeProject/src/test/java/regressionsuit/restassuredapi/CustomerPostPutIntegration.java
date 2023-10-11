411f0bf149190aae1f2523f460f6f4a52064f82e
Aliye Idris <aliyeidiris@gmail.com>
2023-10-11 11:30:01 +0300

    Http Client Api Codes part 3

--- MyPracticeProject/src/test/java/regressionsuit/restassuredapi/CustomerPostPutIntegration.java
+++ MyPracticeProject/src/test/java/regressionsuit/restassuredapi/CustomerPostPutIntegration.java
@@ -1,11 +1,8 @@
 package regressionsuit.restassuredapi;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.JsonToken;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.javafaker.Faker;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -16,47 +13,40 @@ import static io.restassured.RestAssured.given;
 
 public class CustomerPostPutIntegration {
     DataBase db;
-    String path;
     String responseBody;
     int customerId;
     @BeforeClass
     public void setUp(){
         db =new DataBase();
-        path= db.api_host+":"+ db.api_port+"/";
+        RestAssured.baseURI=db.api_host;
+        RestAssured.port=db.api_port;
+        RestAssured.authentication=RestAssured.basic(db.api_username,db.api_password);
     }
     @Test
     public void addCustomerApiTest(){
         CustomerPayload  customerPayload=new CustomerPayload(db.email,db.customerTitle(),db.firstName,db.lastName,0,db.phone,1,"en-GB",db.ipAddress,
                 1);
-        Response response= given().headers("Content-Type","application/json")
-                .and().body(PayloadUtility.getCustomerPayload(customerPayload)).auth().basic(db.api_username,db.api_password)
-                .when().post(path+"customer").then().extract().response();
-
-        response.getBody().prettyPrint();
-        Assert.assertEquals(response.getStatusCode(),200);
-        Assert.assertEquals(response.jsonPath().getString("email"),customerPayload.getEmail());
-        responseBody=response.getBody().asPrettyString();
+        Response response= given().contentType(ContentType.JSON).and().body(customerPayload)
+                .when().post("/customer").then().assertThat().statusCode(200).extract().response();
         customerId=response.jsonPath().getInt("id");
+        response.getBody().prettyPrint();
+        responseBody=response.getBody().asPrettyString();
+
+        Assert.assertEquals(response.jsonPath().getString("email"),customerPayload.getEmail());
+
+
     }
     @Test(description = "update according to response body")
     public void updateCustomerApiTest(){
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode customer;
-        try {
-            customer = mapper.readTree(responseBody);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+
         String newName= Faker.instance().name().firstName();
-        JsonToken editToken=( (ObjectNode) customer).put("firstName",newName).asToken();
         System.out.println("Value to be updated: "+newName);
 
-        Response putResponse= given().headers("Content-Type","application/json")
-                .and().body(customer).auth().basic(db.api_username,db.api_password)
-                .when().put(path+"customer/"+customerId).then()
-                .extract().response();
-        Assert.assertEquals(putResponse.getStatusCode(),204);
+        given().contentType(ContentType.JSON)
+                .and().body(PayloadUtility.updatePayload(responseBody,"firstName",newName))
+                .when().put("/customer/"+customerId).then().assertThat().statusCode(204);
+
         System.out.println("After Update:");
-        Assert.assertTrue(given().when().get(path + "customer/" + customerId).prettyPrint().contains(newName));
+        Assert.assertTrue(given().when().get("/customer/" + customerId).prettyPrint().contains(newName));
     }
 }
--- MyPracticeProject/src/test/java/regressionsuit/restassuredapi/UpdateExistingCustomer.java
+++ MyPracticeProject/src/test/java/regressionsuit/restassuredapi/UpdateExistingCustomer.java
@@ -1,20 +1,11 @@
 package regressionsuit.restassuredapi;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.JsonToken;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.given;
 
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-
-import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import regressionsuit.testngproject.DataBase;
@@ -24,22 +15,20 @@ import java.util.Random;
 
 public class UpdateExistingCustomer {
     DataBase db;
-    String path;
     List<Integer> customerIds;
     int randomId;
-    RequestSpecification specification;
     String responseBody;
     @BeforeClass
     public void setUp(){
         db=new DataBase();
-        path=db.api_host+":"+ db.api_port+"/";
-        specification=given().auth().basic(db.api_username,db.api_password);
+        RestAssured.baseURI=db.api_host;
+        RestAssured.port=db.api_port;
+        RestAssured.authentication=RestAssured.basic(db.api_username,db.api_password);
     }
     @Test(description = "get all customer's id into a list")
     public void getAllCustomerIds() {
-        String responseBody = specification.when().get(path + "customers")
-                .then().assertThat().statusCode(200)
-                .extract().response().asString();
+        String responseBody = given().when().get("/customers")
+                .then().assertThat().statusCode(200).extract().response().asString();
         JsonPath jsonPath = new JsonPath(responseBody);
         customerIds = jsonPath.getList("id");
     }
@@ -47,29 +36,18 @@ public class UpdateExistingCustomer {
     public void getRandomCustomer() {
         Random random = new Random();
         randomId = customerIds.get(random.nextInt(customerIds.size()));
-        Response response = specification.when().get(path + "customer/" + randomId);
-        Assert.assertEquals(response.getStatusCode(),200);
         System.out.println("Before Update:");
-        response.getBody().prettyPrint();
-        responseBody=response.getBody().asPrettyString();
+        responseBody = given().when().get( "/customer/" + randomId)
+                .then().assertThat().statusCode(200).extract().response().prettyPrint();
     }
     @Test(description = "update selected customer's info and print")
     public void updateCustomerInfo(){
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode customer= null;
-        try {
-            customer = mapper.readTree(responseBody);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        JsonToken editToken=( (ObjectNode) customer).put("email",db.email).asToken();
         System.out.println("Value to be updated: "+db.email);
+        given().contentType(ContentType.JSON)
+                .and().body(PayloadUtility.updatePayload(responseBody,"email",db.email))
+                .when().put("/customer/"+randomId).then().assertThat().statusCode(204);
 
-       Response putResponse= RestAssured.given().headers("Content-Type","application/json")
-                .and().body(customer).auth().basic(db.api_username,db.api_password)
-                .when().put(path+"customer/"+randomId).then()
-                .extract().response();
-       Assert.assertEquals(putResponse.getStatusCode(),204);
-        System.out.println("After Update:");specification.when().get(path + "customer/" + randomId).prettyPrint();
+        System.out.println("After Update:");
+        given().when().get("/customer/" + randomId).prettyPrint();
     }
 }
