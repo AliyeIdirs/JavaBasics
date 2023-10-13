1c14563eccef3cd1bf46e9f2429a27cba6c17362
Aliye Idris <aliyeidiris@gmail.com>
2023-10-13 16:24:58 +0300

    Code complement

--- MyPracticeProject/src/test/java/regressionsuit/advancedactions/AdvancedActionsWithJavaScript.java
+++ MyPracticeProject/src/test/java/regressionsuit/advancedactions/AdvancedActionsWithJavaScript.java
@@ -3,6 +3,7 @@ package regressionsuit.advancedactions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -31,6 +32,29 @@ public class AdvancedActionsWithJavaScript {
         }catch (NoSuchElementException e){
             System.out.println("No Such element present.");
         }
+    }
+
+    @Test
+    public void jsClickTest(){
+        driver.get("https://jqueryui.com/dialog/#animated");
+        driver.switchTo().frame(0);
+        WebElement openDialogButton=driver.findElement(By.id("opener"));
+        System.out.println(openDialogButton.getLocation());
+        jse.executeScript("arguments[0].click()",openDialogButton);
+        WebElement basicDialogPopUp=driver.findElement(By.xpath("//span[text()='Basic dialog']"));
+        Assert.assertTrue(basicDialogPopUp.isDisplayed());
+    }
+    @Test
+    public void jsSendKeysTest(){
+        driver.get("http://cubecartuat.unitedcoder.com/admin_tu8sml.php");
+        jse.executeScript("document.getElementById('username').value='testautomation1'");
+        WebElement passwordField=driver.findElement(By.name("password"));
+        jse.executeScript("arguments[0].value='automation123!';",passwordField);
+        String passwordValue=passwordField.getAttribute("value");
+        System.out.println(passwordValue);
+        Assert.assertEquals(passwordValue,"automation123!");
+        WebElement loginButton = driver.findElement(By.id("login"));
+        jse.executeScript("arguments[0].click()",loginButton);
     }
     @AfterClass
     public void tearDown(){
