ef40f034e83519579a25a1fd6019582b5b95786f
Aliye Idris <aliyeidiris@gmail.com>
2023-11-25 19:14:11 +0300

    js executor

--- MyPracticeProject/src/main/java/com/unitedcoder/uiautomation/GoogleSearch.java
+++ MyPracticeProject/src/main/java/com/unitedcoder/uiautomation/GoogleSearch.java
@@ -1,26 +1,28 @@
 package com.unitedcoder.uiautomation;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.PageLoadStrategy;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
+
 public class GoogleSearch {
     public static void main(String[] args) throws InterruptedException {
 
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-        ChromeDriver driver = new ChromeDriver(chromeOptions);
+        WebDriverManager.safaridriver().setup();
+        SafariOptions safariOptions = new SafariOptions();
+        safariOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        WebDriver driver = new SafariDriver(safariOptions);
 
         //Set browser size
         driver.navigate().to("https://www.google.com");
         Dimension dimension= new Dimension(700,800);
         driver.manage().window().setSize(dimension);
         driver.manage().window().maximize();
-        driver.close();
+        driver.findElement(By.id("APjFqb")).sendKeys("What is SDET");
+        Actions actions=new Actions(driver);
+        actions.keyDown(Keys.ENTER).perform();
+        Thread.sleep(5000);
         driver.quit();
     }
 }
